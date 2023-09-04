40ce3e9375949e05703f2e318b158515ff2a8bf3
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-09-04 15:36:23 -0300

    Add files via upload

---
+++ quest02.py
@@ -0,0 +1,10 @@
+
+class ArvoreBinaria:
+    def __init__(self):
+        self.raiz = None
+
+    def inserir_em_nivel(self, valor):
+        if self.raiz is None:
+            self.raiz = No(valor)
+        else:
+            self.inserir_em_nivel_recursivo(valor, self.raiz)
