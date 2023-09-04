ea8c0b0d34aa85553bff27b22fb982b91f2f7b5d
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-09-04 15:39:33 -0300

    Add files via upload

---
+++ quest08.py
@@ -0,0 +1,5 @@
+    def inserir_em_nivel(self, valor):
+        if self.raiz is None:
+            self.raiz = No(valor)
+        else:
+            self.inserir_em_nivel_recursivo(valor, self.raiz)
