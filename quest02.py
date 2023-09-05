1b8f0aef853313c213bff03039560fac2603704e
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-09-05 15:56:44 -0300

    Update quest02.py

--- quest02.py
+++ quest02.py
@@ -1,17 +1,40 @@
-def insere(self, valor):
+class No:
+    def __init__(self, valor):
+        self.valor = valor
+        self.esquerda = None
+        self.direita = None
+
+class BinaryTree:
+    def __init__(self):
+        self.raiz = None
+
+    def insere(self, valor):
         if self.raiz is None:
             self.raiz = No(valor)
         else:
-            self._insere_recursivamente(self.raiz,valor)
+            self._insere_recursivamente(self.raiz, valor)
 
     def _insere_recursivamente(self, no_atual, valor):
-        if valor < no_atual.chave:
+        if valor < no_atual.valor:
             if no_atual.esquerda is None:
                 no_atual.esquerda = No(valor)
             else:
                 self._insere_recursivamente(no_atual.esquerda, valor)
-        elif valor > no_atual.chave:
+        elif valor > no_atual.valor:
             if no_atual.direita is None:
                 no_atual.direita = No(valor)
             else:
                 self._insere_recursivamente(no_atual.direita, valor)
+
+   
+
+# Exemplo de uso:
+if __name__ == "__main__":
+    arvore = BinaryTree()
+    numeros = [11, 5, 17, 3, 7, 12, 20]
+
+    for numero in numeros:
+        arvore.insere(numero)
+
+    print("Árvore Binária Criada!")
+    print(numeros)
