2a29c0109fd2d419c52dcd0d7b26f713ecfc15eb
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-09-05 10:09:52 -0300

    Update quest02.py

--- quest02.py
+++ quest02.py
@@ -1,10 +1,17 @@
-
-class ArvoreBinaria:
-    def __init__(self):
-        self.raiz = None
-
-    def inserir_em_nivel(self, valor):
+def insere(self, valor):
         if self.raiz is None:
             self.raiz = No(valor)
         else:
-            self.inserir_em_nivel_recursivo(valor, self.raiz)
+            self._insere_recursivamente(self.raiz,valor)
+
+    def _insere_recursivamente(self, no_atual, valor):
+        if valor < no_atual.chave:
+            if no_atual.esquerda is None:
+                no_atual.esquerda = No(valor)
+            else:
+                self._insere_recursivamente(no_atual.esquerda, valor)
+        elif valor > no_atual.chave:
+            if no_atual.direita is None:
+                no_atual.direita = No(valor)
+            else:
+                self._insere_recursivamente(no_atual.direita, valor)
