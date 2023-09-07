9c3ea63ac66aaa5bdaa46f99e6de0199ce409fc3
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-09-07 10:47:17 -0300

    Update quest08.py

--- quest08.py
+++ quest08.py
@@ -25,3 +25,43 @@ class ArvoreBinaria:
                 no_atual.direita = No(valor)
             else:
                 self._insere_recursivamente(no_atual.direita, valor)
+
+    def travessia_em_niveis(self):
+        if self.raiz is None:
+            return []
+
+        resultado = []
+        nivel_atual = 0
+
+        while True:
+            nivel = self._travessia_nivel(self.raiz, nivel_atual, [])
+            if nivel:
+                resultado.extend(nivel)
+                nivel_atual += 1
+            else:
+                break
+
+        return resultado
+
+    def _travessia_nivel(self, no_atual, nivel_alvo, resultado_nivel):
+        if no_atual is None:
+            return []
+
+        if nivel_alvo == 0:
+            resultado_nivel.append(no_atual.valor)
+        elif nivel_alvo > 0:
+            esquerda = self._travessia_nivel(no_atual.esquerda, nivel_alvo - 1, resultado_nivel)
+            direita = self._travessia_nivel(no_atual.direita, nivel_alvo - 1, resultado_nivel)
+
+        return resultado_nivel
+
+
+if __name__ == "__main__":
+    arvoreB = ArvoreBinaria()
+    numeros = [12, 5, 18, 3, 9, 12, 20]
+
+    for numero in numeros:
+        arvoreB.insere(numero)
+
+    resultado_travessia = arvoreB.travessia_em_niveis()
+    print("Travessia em Níveis:", resultado_travessia)
