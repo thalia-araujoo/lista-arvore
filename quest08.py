3deb008eeb0b7017be27f0df59546a9fbe6f697e
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-09-07 10:13:10 -0300

    Update quest08.py

--- quest08.py
+++ quest08.py
@@ -1,5 +1,27 @@
-    def inserir_em_nivel(self, valor):
+class No:
+    def __init__(self, valor):
+        self.valor = valor
+        self.esquerda = None
+        self.direita = None
+
+class ArvoreBinaria:
+    def __init__(self):
+        self.raiz = None
+
+    def insere(self, valor):
         if self.raiz is None:
             self.raiz = No(valor)
         else:
-            self.inserir_em_nivel_recursivo(valor, self.raiz)
+            self._insere_recursivamente(self.raiz, valor)
+
+    def _insere_recursivamente(self, no_atual, valor):
+        if valor < no_atual.valor:
+            if no_atual.esquerda is None:
+                no_atual.esquerda = No(valor)
+            else:
+                self._insere_recursivamente(no_atual.esquerda, valor)
+        elif valor > no_atual.valor:
+            if no_atual.direita is None:
+                no_atual.direita = No(valor)
+            else:
+                self._insere_recursivamente(no_atual.direita, valor)
